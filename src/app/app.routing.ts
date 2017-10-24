2f1a7e52f83b89eff96815a9cde45b9905c38cac
valbercarreiro <valbercarreiro@gmail.com>
2017-10-24 20:49:46 -0300

    angular 4 - loiane training

---
+++ src/app/app.routing.ts
@@ -0,0 +1,14 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { CursosComponent } from './cursos/cursos.component';
+import { LoginComponent } from './login/login.component';
+import { HomeComponent } from './home/home.component';
+
+const APP_ROUTES: Routes = [
+    { path: 'cursos', component: CursosComponent },
+    { path: 'login', component: LoginComponent },
+    { path : '', component: HomeComponent }
+];
+
+export const routing: ModuleWithProviders = RouterModule.forRoot(APP_ROUTES);
