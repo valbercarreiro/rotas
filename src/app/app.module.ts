9c1d26e949f8329ee6ddb75737402cc17e342857
valbercarreiro <valbercarreiro@gmail.com>
2017-11-16 20:15:26 -0300

    angular 4 - loiane trainning

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,8 +12,10 @@ import { LoginComponent } from './login/login.component';
 //import { CursosService } from './cursos/cursos.service';
 //import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 import { AppRoutingModule } from './app.routing.module';
-import { CursosModule } from './cursos/cursos.module';
-import { AlunosModule } from './alunos/alunos.module';
+//import { CursosModule } from './cursos/cursos.module';
+//import { AlunosModule } from './alunos/alunos.module';
+import { AuthService } from './login/auth.service';
+
 
 @NgModule({
   declarations: [
@@ -29,12 +31,13 @@ import { AlunosModule } from './alunos/alunos.module';
     FormsModule,
     HttpModule,
     //routing
-    CursosModule,
-    AlunosModule,
+    //CursosModule,
+    //AlunosModule,
     AppRoutingModule
   ],
   providers: [
     //CursosService
+    AuthService
   ],
   bootstrap: [AppComponent]
 })
